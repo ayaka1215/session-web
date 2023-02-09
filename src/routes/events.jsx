0b6dad4945c0def9e5ad1050c1ff42fb8ea148ec
AyakaOoshima <och1215bi3@gmail.com>
2023-02-09 17:48:09 +0900

    カードレイアウト調整

--- src/routes/events.jsx
+++ src/routes/events.jsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { axiosInstance } from "../utils/axios.js";
-// import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Card, Heading, Text, Image, Stack, CardBody, ButtonGroup, Button, CardFooter } from '@chakra-ui/react'
 
 function Events() {
     const [Events, setEvents] = useState();
@@ -14,19 +13,38 @@ function Events() {
    }, []);
  
     return (
-        <div style={{ margin: "auto", width: "1000px" }}>
+        <div>
             <h1>イベント一覧画面</h1>
             <div>
-                <Link to="/">TOP</Link>
-            </div>
-            <div>
                 {Events?.map((e) => {
-                    return (
-                            <div>
-                                <p>{e.title}</p>
-                                <p>{e.start_time} - {e.end_time}</p>
-                                <p>{e.place}</p>
-                            </div>
+                    return (                        
+                        <Card maxW='sm'>
+                            <CardBody>
+                                <Image
+                                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                                    alt='Green double couch with wooden legs'
+                                    borderRadius='lg'
+                                />
+                                <Stack mt='6' spacing='3'>
+                                    <Heading size='md'>{e.title}</Heading>
+                                    <Text>{e.start_time} - {e.end_time}</Text>
+                                    <Text>@{e.place}</Text>
+                                </Stack>
+                            </CardBody>
+                            <CardFooter>
+                                <ButtonGroup spacing='2'>
+                                <Button variant='solid' colorScheme='blue'>
+                                    詳細
+                                </Button>
+                                <Button variant='ghost' colorScheme='blue'>
+                                    編集
+                                </Button>
+                                <Button variant='ghost' colorScheme='blue'>
+                                    削除
+                                </Button>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
                         );
                     })}
             </div>
