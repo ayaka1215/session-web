987f90a8116aac79d0248a1867bfd5125d4ede9a
AyakaOoshima <och1215bi3@gmail.com>
2023-02-10 22:21:43 +0900

    feat: CreateEventルーティング設定

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./routes/events.jsx";
-import './App.css';
+import CreateEvent from "./routes/createEvent.jsx";
+import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="events" element={<Events />}/>
+        <Route path="events" element={<Events />} />
+        <Route path="events/create" element={<CreateEvent />} />
       </Routes>
     </BrowserRouter>
   );
