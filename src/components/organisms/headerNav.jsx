6f2c79d13530ffd0bc0e35d385a525eab387e846
AyakaOoshima <och1215bi3@gmail.com>
2023-02-10 22:32:11 +0900

    feat: イベント一覧リンク設定

--- src/components/organisms/headerNav.jsx
+++ src/components/organisms/headerNav.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { Flex, Text, Box, Heading } from "@chakra-ui/react";
 
 function HeaderNav() {
   return (
     <Flex minWidth="max-content" alignItems="center" gap="2">
       <Box p="2">
-        <Heading size="md">Session App</Heading>
+        <Heading size="md">
+          <Link to="/events">Session App</Link>
+        </Heading>
       </Box>
       <Text>マイページ</Text>
       <Text>予約一覧</Text>
