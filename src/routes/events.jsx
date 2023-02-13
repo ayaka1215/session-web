a54686fc278c3e11c78a08994b71376ee0573792
AyakaOoshima <och1215bi3@gmail.com>
2023-02-13 20:25:08 +0900

    feat: アップロード画像表示設定

--- src/routes/events.jsx
+++ src/routes/events.jsx
@@ -38,11 +38,21 @@ function Events() {
             return (
               <Card maxW="sm">
                 <CardBody>
-                  <Image
-                    src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="lg"
-                  />
+                  {e.image?.url ? (
+                    <Image
+                      src={e.image.url}
+                      alt={e.title}
+                      borderRadius="lg"
+                      width="100%"
+                      maxHeight="178px"
+                    />
+                  ) : (
+                    <Image
+                      src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                      alt={e.title}
+                      borderRadius="lg"
+                    />
+                  )}
                   <Stack mt="6" spacing="2">
                     <Heading size="md">{e.title}</Heading>
                     <Text>
