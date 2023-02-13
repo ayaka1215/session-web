07ad226081f243c21fd978895c52a8206ca4835b
AyakaOoshima <och1215bi3@gmail.com>
2023-02-13 18:09:12 +0900

    fix: エラー処理実装

--- src/routes/createEvent.jsx
+++ src/routes/createEvent.jsx
@@ -9,6 +9,7 @@ import {
   Textarea,
   Button,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import HeaderNav from "../components/organisms/HeaderNav.jsx";
 
@@ -16,10 +17,11 @@ function CreateEvent() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [date, setDate] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
+  const [start_time, setStartTime] = useState("");
+  const [end_time, setEndTime] = useState("");
   const [place, setPlace] = useState("");
   const navigate = useNavigate();
+  const toast = useToast();
 
   const onClick = async () => {
     try {
@@ -27,13 +29,27 @@ function CreateEvent() {
         title,
         content,
         date,
-        startTime,
-        endTime,
+        start_time,
+        end_time,
         place,
       });
       navigate("/events", { replace: true });
+      toast({
+        title: "イベントを作成しました。",
+        description: "",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
     } catch (e) {
       console.log(e);
+      toast({
+        title: "エラーが発生しました。",
+        description: "入力内容を確認してください。",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
     }
   };
 
@@ -42,9 +58,8 @@ function CreateEvent() {
       <HeaderNav />
       <div style={{ margin: "auto", maxWidth: "1000px" }}>
         <Heading as="h1" size="lg" noOfLines={1} mb="10" mt="5" ml="2">
-          イベント新規
+          イベント新規作成
         </Heading>
-
         <VStack spacing={6} mx={2}>
           <FormControl isRequired>
             <FormLabel>イベントタイトル</FormLabel>
@@ -55,7 +70,6 @@ function CreateEvent() {
               placeholder="イベントタイトルを入力してください。"
             />
           </FormControl>
-
           <FormControl isRequired>
             <FormLabel>内容</FormLabel>
             <Textarea
@@ -65,7 +79,6 @@ function CreateEvent() {
               placeholder="イベント内容を入力してください。"
             />
           </FormControl>
-
           <FormControl isRequired>
             <FormLabel>開催日</FormLabel>
             <Input
@@ -75,27 +88,24 @@ function CreateEvent() {
               onChange={(e) => setDate(e.target.value)}
             />
           </FormControl>
-
           <FormControl isRequired>
             <FormLabel>開始時刻</FormLabel>
             <Input
               maxWidth="200px"
               type="time"
-              value={startTime}
+              value={start_time}
               onChange={(e) => setStartTime(e.target.value)}
             />
           </FormControl>
-
           <FormControl isRequired>
             <FormLabel>終了時刻</FormLabel>
             <Input
               maxWidth="200px"
               type="time"
-              value={endTime}
+              value={end_time}
               onChange={(e) => setEndTime(e.target.value)}
             />
           </FormControl>
-
           <FormControl isRequired>
             <FormLabel>開催場所</FormLabel>
             <Input
@@ -105,7 +115,6 @@ function CreateEvent() {
               placeholder="開催場所を入力してください。"
             />
           </FormControl>
-
           <Button colorScheme="teal" onClick={onClick} width="300px" mt="5">
             作成する
           </Button>
