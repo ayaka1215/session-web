44d48ebb65a7ec2d4f4fb31b967b07e8ccdd1971
AyakaOoshima <och1215bi3@gmail.com>
2023-02-13 20:31:00 +0900

    feat: イベントnoimage設定

--- src/routes/events.jsx
+++ src/routes/events.jsx
@@ -44,11 +44,11 @@ function Events() {
                       alt={e.title}
                       borderRadius="lg"
                       width="100%"
-                      maxHeight="178px"
+                      maxHeight="156px"
                     />
                   ) : (
                     <Image
-                      src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                      src={`${process.env.PUBLIC_URL}/event_no_image.png`}
                       alt={e.title}
                       borderRadius="lg"
                     />
