444be03bb60a73bc84ed7036dbe6f6e1faedd7b1
AyakaOoshima <och1215bi3@gmail.com>
2023-02-15 21:12:41 +0900

    feat: レイアウト調整

--- src/routes/event.jsx
+++ src/routes/event.jsx
@@ -14,6 +14,8 @@ import {
   SimpleGrid,
   Flex,
   Spacer,
+  Center,
+  Box,
 } from "@chakra-ui/react";
 import Common from "../components/layout/Common.jsx";
 import { format } from "date-fns";
@@ -34,11 +36,8 @@ function Event() {
   return (
     <>
       <Common>
-        <div style={{ margin: "auto", maxWidth: "1000px" }}>
+        <div style={{ margin: "auto", maxWidth: "650px", padding: "5px" }}>
           <Flex mb="5" mt="5">
-            <Heading as="h1" size="lg" noOfLines={1} ml="5">
-              イベント詳細
-            </Heading>
             <Spacer />
             <Button variant="ghost" colorScheme="blue">
               編集
@@ -47,46 +46,58 @@ function Event() {
               削除
             </Button>
           </Flex>
-          <SimpleGrid columns={[1, null, 3]} spacingX="40px" spacingY="20px">
-            <Card maxW="sm">
-              {event && (
-                <CardBody>
-                  {event.image?.url ? (
-                    <Image
-                      src={event.image.url}
-                      alt={event.title}
-                      borderRadius="lg"
-                      width="100%"
-                      maxHeight="156px"
-                    />
-                  ) : (
-                    <Image
-                      src={`${process.env.PUBLIC_URL}/event_no_image.png`}
-                      alt={event.title}
-                      borderRadius="lg"
-                    />
-                  )}
-                  <Stack mt="6" spacing="2">
-                    <Heading size="md">{event.title}</Heading>
-                    <Text>
-                      {format(new Date(event.date), "yyyy年M月d日", {
-                        locale: ja,
-                      })}{" "}
-                      {format(new Date(event.start_time), "HH:mm", {
-                        locale: ja,
-                      })}{" "}
-                      -{" "}
-                      {format(new Date(event.end_time), "HH:mm", {
-                        locale: ja,
-                      })}
-                    </Text>
-                    <Text>@{event.place}</Text>
-                    <Text>{event.content}</Text>
-                  </Stack>
-                </CardBody>
+          {event && (
+            <>
+              {event.image?.url ? (
+                <Image
+                  src={event.image.url}
+                  alt={event.title}
+                  borderRadius="lg"
+                  maxWidth="100%"
+                  maxHeight="156px"
+                />
+              ) : (
+                <Image
+                  src={`${process.env.PUBLIC_URL}/event_no_image.png`}
+                  alt={event.title}
+                  borderRadius="lg"
+                  maxWidth="100%"
+                />
               )}
-            </Card>
-          </SimpleGrid>
+              <Stack mt="6" mx="2" spacing="2">
+                <Heading size="md">{event.title}</Heading>
+                <Text>
+                  {format(new Date(event.date), "yyyy年M月d日", {
+                    locale: ja,
+                  })}{" "}
+                  {format(new Date(event.start_time), "HH:mm", {
+                    locale: ja,
+                  })}{" "}
+                  -{" "}
+                  {format(new Date(event.end_time), "HH:mm", {
+                    locale: ja,
+                  })}
+                </Text>
+                <Text>@{event.place}</Text>
+              </Stack>
+              <Box
+                mt="6"
+                bg={"gray.100"}
+                rounded="md"
+                p="5"
+                border="1px"
+                borderColor="gray.100"
+              >
+                <Text fontWeight="bold">イベント概要</Text>
+                <Text mt="5">{event.content}</Text>
+              </Box>
+              <Center>
+                <Button colorScheme="teal" width="300px" my="10">
+                  このイベントを予約する
+                </Button>
+              </Center>
+            </>
+          )}
         </div>
       </Common>
     </>
