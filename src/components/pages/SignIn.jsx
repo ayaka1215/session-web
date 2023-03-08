66cc5260ed3e7647180f7b2f1e1b30a139ef4ee3
AyakaOoshima <och1215bi3@gmail.com>
2023-03-08 16:49:50 +0900

    fix: レイアウト調整

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -1,47 +1,25 @@
 import React, { useState, useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
-import TextField from "@material-ui/core/TextField";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import Button from "@material-ui/core/Button";
-import Box from "@material-ui/core/Box";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  Container,
+} from "@chakra-ui/react";
 
 import { AuthContext } from "../../App.js";
 import AlertMessage from "../utils/AlertMessage.jsx";
 import { signIn } from "../../lib/apiClient/auth.js";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: theme.spacing(6),
-  },
-  submitBtn: {
-    marginTop: theme.spacing(2),
-    flexGrow: 1,
-    textTransform: "none",
-  },
-  header: {
-    textAlign: "center",
-  },
-  card: {
-    padding: theme.spacing(2),
-    maxWidth: 400,
-  },
-  box: {
-    marginTop: "2rem",
-  },
-  link: {
-    textDecoration: "none",
-  },
-}));
-
-// サインイン用ページ
 const SignIn = () => {
-  const classes = useStyles();
   const navigate = useNavigate();
 
   const { setIsSignedIn, setCurrentUser } = useContext(AuthContext);
@@ -81,54 +59,83 @@ const SignIn = () => {
 
   return (
     <>
-      <form noValidate autoComplete="off">
-        <Card className={classes.card}>
-          <CardHeader className={classes.header} title="Sign In" />
-          <CardContent>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              label="Email"
-              value={email}
-              margin="dense"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              label="Password"
-              type="password"
-              placeholder="At least 6 characters"
-              value={password}
-              margin="dense"
-              autoComplete="current-password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              size="large"
-              fullWidth
-              color="default"
-              disabled={!email || !password ? true : false} // 空欄があった場合はボタンを押せないように
-              className={classes.submitBtn}
-              onClick={handleSubmit}
-            >
-              Submit
-            </Button>
-            <Box textAlign="center" className={classes.box}>
-              <Typography variant="body2">
-                Don't have an account? &nbsp;
-                <Link to="/signup" className={classes.link}>
+      <Container
+        maxW="lg"
+        py={{ base: "12", md: "24" }}
+        px={{ base: "0", sm: "8" }}
+      >
+        <Stack spacing="8">
+          <Stack spacing="6">
+            <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+              <Heading size="lx" as="h2">
+                Sign In
+              </Heading>
+              <HStack spacing="1" justify="center">
+                <Text color="muted">Don't have an account?</Text>
+                <Button variant="link" colorScheme="blue" to="/signup">
                   Sign Up now!
-                </Link>
-              </Typography>
-            </Box>
-          </CardContent>
-        </Card>
-      </form>
+                </Button>
+              </HStack>
+            </Stack>
+          </Stack>
+          <Box
+            py={{ base: "0", sm: "8" }}
+            px={{ base: "4", sm: "10" }}
+            bg={{ base: "transparent", sm: "bg-surface" }}
+            boxShadow={{ base: "none", sm: "md" }}
+            borderRadius={{ base: "none", sm: "xl" }}
+          >
+            <Stack spacing="6">
+              <Stack spacing="5">
+                <FormControl>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Input
+                    border="1px"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    required
+                    label="Email"
+                    value={email}
+                    margin="dense"
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Input
+                    border="1px"
+                    id="password"
+                    variant="outlined"
+                    required
+                    label="Password"
+                    type="password"
+                    placeholder="At least 6 characters"
+                    value={password}
+                    margin="dense"
+                    autoComplete="current-password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </FormControl>
+                <Stack spacing="6">
+                  <Button
+                    type="submit"
+                    size="large"
+                    colorScheme="teal"
+                    fullWidth
+                    onClick={handleSubmit}
+                    mt="5"
+                    py="3"
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
+
       <AlertMessage // エラーが発生した場合はアラートを表示
         open={alertMessageOpen}
         setOpen={setAlertMessageOpen}
